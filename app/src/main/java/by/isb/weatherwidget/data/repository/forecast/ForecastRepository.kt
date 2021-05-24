a951a6100742660cdae4dc96717739de992b8ec2
Igor <berikz@gmail.com>
2021-05-24 22:04:57 +0300

    add data api

--- app/src/main/java/by/isb/weatherwidget/data/repository/forecast/ForecastRepository.kt
+++ app/src/main/java/by/isb/weatherwidget/data/repository/forecast/ForecastRepository.kt
@@ -3,6 +3,7 @@ package by.isb.weatherwidget.data.repository.forecast
 import by.isb.an07.hw8.data.mappers.crypto.CryptoResponseMapper
 import by.isb.an07.hw8.data.entities.crypto.Crypto
 import by.isb.an07.hw8.data.networking.crypto.CryptoApi
+import by.isb.weatherwidget.data.entities.forecast.Forecast
 import by.isb.weatherwidget.data.mappers.forecast.ForecastResponseMapper
 import by.isb.weatherwidget.data.networking.forecast.ForecastApi
 
@@ -12,7 +13,7 @@ class ForecastRepository {
     private val forecastResponseMapper = ForecastResponseMapper()
 
     suspend fun loadForecast(lat: Double, lon: Double, units: String): List<Forecast> {
-        val response = api.loadCrypto(sort,sortDir)
+        val response = api.loadForecast(lat,lon,units)
 
         return if (response.isSuccessful) {
             response.body()?.dataIn?.map {
